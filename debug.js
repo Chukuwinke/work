6b988a809b7e60c77bee860bc07ebced260d2099
Chukuwinke <rittnerodigwe@gmail.com>
2021-09-15 01:04:29 +0300

    worked on load image button

--- debug.js
+++ debug.js
@@ -1,91 +1,137 @@
-
-const tabs = document.querySelector('.tabs')
-const workImgContainer = document.querySelector('.work-tabs-img__container')
+const tabs = document.querySelector(".tabs");
+const workButton = document.getElementById('work__btn')
+const workImgContainer = document.querySelector(".work-tabs-img__container");
 let linkName;
-    $('.services-tabs-content li').first().css('display', 'flex')
-    $('.services-tabs__title').click(function(){
-        $('.services-tabs__title.tab-active').removeClass('tab-active');
-        $(this).addClass('tab-active');
-        linkName = $(this).attr("data-tab-switch");
-        console.log(linkName)
-        $('.services-tabs-content li').css('display', 'none');
-        $('#'+linkName).css('display', 'flex ');
-    })
-    
+$(".services-tabs-content li").first().css("display", "flex");
+$(".services-tabs__title").click(function () {
+  $(".services-tabs__title.tab-active").removeClass("tab-active");
+  $(this).addClass("tab-active");
+  linkName = $(this).attr("data-tab-switch");
+  console.log(linkName);
+  $(".services-tabs-content li").css("display", "none");
+  $("#" + linkName).css("display", "flex ");
+});
+
 // new work
 
-fetch('pictures.json')
-  .then(response => response.json())
-  .then(data => {
-
+fetch("pictures.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let links = Object.values(data.images);
     //function to populate work image container with images
-   const tabSwitcher = (target) => {
-    let linkTab = target.getAttribute("data-work-tab")
-    let links = Object.values(data.images)
-    .filter(items => items.category == linkTab)
-    .map(item => item.url)
-    
-    links.forEach(element => {
-      let imageWrapper = document.createElement('div')
-      console.log(element)
-      imageWrapper.innerHTML = `<img class="product-image" src=${element} />`
-      workImgContainer.appendChild(imageWrapper)
-    });
-    
-   }
-   // clicking a button targets one of the tabs
+    const imageBox = (links, imgItems) => {
+      if (document.contains(imgItems)) {
+        //console.log('works')
+        document.querySelectorAll(".imgStuff").forEach((e) => e.remove());
+      }
+      links.forEach((element) => {
+        //console.log(element)
+        let image = document.createElement("img");
+        image.setAttribute("src", element);
+        image.className += "imgStuff";
+        workImgContainer.appendChild(image);
+      });
+    };
+    const tabSwitcher = (links, target) => {
+      let imgItems = document.querySelector(".imgStuff");
+      let linkTab = target.getAttribute("data-work-tab");
+
+      if (linkTab == "all") {
+        //console.log('alll')
+        let linkArr = [];
+        links.map((item) => item);
+        const ellinks = [...links];
+        let currentItems = 0;
+        for (let i = currentItems; i < currentItems + 12; i++) {
+          linkArr.push(ellinks[i].url);
+        }
+        imageBox(linkArr, imgItems);
+        workButton.style.display ='initial'
+      } else {
+        let filteredLinks = links
+          .filter((items) => items.category == linkTab)
+          .map((item) => item.url);
+        imageBox(filteredLinks, imgItems);
+        workButton.style.display ='none'
+      }
+    };
+
+    // clicking a button targets one of the tabs
     tabs.onclick = (event) => {
       let target = event.target;
+      if (target.className != "tabs__title") return;
+      tabSwitcher(links, target);
+    };
+
+    let defArr = [];
+    let alldefault = [...links.map((item) => item)];
+    let currentItems = 0;
+    for (let i = currentItems; i < currentItems + 12; i++) {
+      defArr.push(alldefault[i].url);
+    }
+    currentItems += defArr.length
     
-      if(target.className != 'tabs__title') return;
-      tabSwitcher(target);
+    defArr.forEach((element) => {
+      //console.log(element);
+      let image = document.createElement("img");
+      image.setAttribute("src", element);
+      image.className += "imgStuff";
+      workImgContainer.appendChild(image);
+    });
+    workButton.onclick = (event) => {
+      let newdef = [];
+      for (let i = currentItems; i < currentItems + 12; i++) {
+          newdef.push(alldefault[i].url)
+
+        
+        console.log(currentItems)
+      }
+      newdef.forEach(element => {
+        let image = document.createElement("img");
+        image.setAttribute("src", element);
+        image.className += "imgStuff";
+        workImgContainer.appendChild(image);
+      });
+      currentItems += newdef.length
+      if(currentItems >= 24){
+        event.target.style.display = 'none'
+        currentItems = 12
+      }
+      //console.log(newdef);
     }
   })
-  .catch(error => console.log(error));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  .catch((error) => console.log(error));
 
 /**
  * IGNORE
  */
 
-   //console.log(json)
+//  let linkArr =[];
+//  links = Object.values(data.images).map(item => item)
+//  const ellinks = [...links]
+//  let currentItems = 12;
+//  for (let i = currentItems; i < currentItems + 12; i++) {
+//    linkArr.push(ellinks[i].url)
+//  }
+//  console.log(linkArr)
+
+//console.log(json)
 //console.log(Object.values(json).filter(items => items.url));
 //<img src="../Step Project Ham Pictures/Step Project Ham/graphic design/graphic-design1.jpg" alt="">
 //console.log(Object.values(images).filter(image => image.category == "wordpress"));
 
+// $.getJSON("pictures.json", function(json) {
+//   let imagelist ='';
+//   $.each(json.images, function () {
+//     imagelist += `<img src= "${this.url}">`;
+//     console.log(this.url)
+//   })
+//   $(".work-tabs-img__container").append(imagelist)
 
+//   console.log(json.images); // this will show the info it in firebug console
+// });
 
-
-  // $.getJSON("pictures.json", function(json) {
-    //   let imagelist ='';
-    //   $.each(json.images, function () {
-    //     imagelist += `<img src= "${this.url}">`;
-    //     console.log(this.url)
-    //   })
-    //   $(".work-tabs-img__container").append(imagelist)
-
-    //   console.log(json.images); // this will show the info it in firebug console
-    // });
-
-    // const images = {
+// const images = {
 //   "1": {
 //     "name": "",
 //     "category": "webDev",
